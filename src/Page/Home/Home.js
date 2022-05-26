d162bada53d8da661d914d19461888bd8256231d
rahathossain3 <rahathossain210@gmail.com>
2022-05-26 10:06:17 +0600

    product update

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import Products from './Products';
 
 const Home = () => {
     return (
         <div>
             <Banner />
 
-
+            <Products></Products>
 
 
         </div>
---
+++ src/Page/Home/Products.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <section className='mt-8 mb-8'>
+            <h2 className='text-5xl text-center text-primary mt-10 font-semibold mb-8'> Buy Best for Your Camera</h2>
+
+            <div>
+
+            </div>
+        </section>
+    );
+};
+
+export default Products;
