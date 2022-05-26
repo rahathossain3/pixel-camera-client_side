92261d00e2e9d593fdffc5aae5da2eb89bb6ea23
rahathossain3 <rahathossain210@gmail.com>
2022-05-26 19:12:52 +0600

    product add

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Banner from './Banner/Banner';
-import Products from './Products';
+import HomeProducts from './HomeProducts';
 
 const Home = () => {
     return (
         <div>
             <Banner />
 
-            <Products></Products>
+            <HomeProducts></HomeProducts>
 
 
         </div>
