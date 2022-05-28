0b15e8308ecc13ffc10193c297f9e5c680b968f2
rahathossain3 <rahathossain210@gmail.com>
2022-05-28 09:38:12 +0600

    sucefullly product delete by modal

---
+++ src/Page/Dashboard/DeleteConfirmModal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const DeleteConfirmModal = ({ deletingProduct, refetch, setDeletingDoctor: setDeletingProduct }) => {
+
+    // const { name, email } = deletingProduct;
+    const { _id, name, img, details, minimumQnt, availableQnt, unitPrice, email } = deletingProduct;
+
+
+    const handleDelete = (id) => {
+        fetch(`http://localhost:5000/product/${id}`, {
+            method: 'DELETE',
+            // headers: {
+            //     authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            // }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount) {
+                    toast.success(`Product: ${name} is deleted.`)
+                    setDeletingProduct(null);
+                    refetch();
+                }
+            })
+    }
+    return (
+        <div>
+            <input type="checkbox" id="delete-confirm-modal" class="modal-toggle" />
+            <div class="modal modal-bottom sm:modal-middle">
+                <div class="modal-box">
+                    <h3 class="font-bold text-lg text-red-500">Are you sure you want to delete  ${name}!</h3>
+                    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <div class="modal-action">
+                        <button onClick={() => handleDelete(_id)} class="btn btn-xs btn-error">Delete</button>
+                        <label htmlfor="delete-confirm-modal" class="btn btn-xs">Cancel</label>
+                    </div>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default DeleteConfirmModal;
--- src/Page/Dashboard/ManageProduct.js
+++ src/Page/Dashboard/ManageProduct.js
@@ -1,9 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../Shared/Loading/Loading';
+
+import DeleteConfirmModal from './DeleteConfirmModal';
+import ProductRow from './ProductRow';
 
 const ManageProduct = () => {
+
+    //modal
+    const [deletingProduct, setDeletingProduct] = useState('');
+
+    //react query 
+    const { data: products, isLoading, refetch } = useQuery('products', () => fetch('http://localhost:5000/product', {
+        // headers: {
+        //     authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        // }
+    }).then(res => res.json()))
+
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
-        <div>
-            <h2>this is from manage Product</h2>
+        <div className='container text-center pt-4'>
+
+
+            <hr />
+            <h2 className='text-5xl mb-4 text-center mt-3'>this is from manage Product</h2>
+
+            <h2 className="text-2xl">Manage Doctor: {products.length}</h2>
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Avatar</th>
+                            <th>Name</th>
+                            <th>Minimum quantity/Available quantity </th>
+                            <th>Product Price $</th>
+                            <th>Action</th>
+                            <th>Supplier Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            products.map((product, index) => <ProductRow
+                                key={product._key}
+                                product={product}
+                                index={index}
+                                refetch={refetch}
+                                setDeletingProduct={setDeletingProduct}
+                            > </ProductRow>)
+                        }
+                    </tbody>
+
+                </table>
+            </div>
+            {deletingProduct && <DeleteConfirmModal
+                deletingProduct={deletingProduct}
+                refetch={refetch}
+                setDeletingDoctor={setDeletingProduct}
+            ></DeleteConfirmModal>}
         </div>
     );
 };
